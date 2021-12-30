fd5dfef77db44db4a3a4f65ff12415a4fdc846df
H Mauricio García <30942047+MauriciioGS@users.noreply.github.com>
2021-12-30 15:59:55 -0600

    Upload project

---
+++ main.py
@@ -0,0 +1,76 @@
+from fastapi import FastAPI,status,HTTPException
+from pydantic import BaseModel
+from typing import List
+from database import SessionLocal
+import models
+
+app = FastAPI()
+
+class Tool(BaseModel):
+    id:int
+    title:str
+    description:str
+    image:str
+    link:str
+
+    class Config:
+        orm_mode=True
+
+db = SessionLocal()
+
+@app.get('/tools',response_model=List[Tool],
+        status_code=status.HTTP_200_OK)
+def get_all_tools():
+    tools = db.query(models.Tool).all()
+    return tools
+
+@app.get('/tool/{tool_id}', response_model=Tool,
+        status_code=status.HTTP_200_OK)
+def get_an_tool(tool_id:int):
+    tool = db.query(models.Tool).filter(models.Tool.id == tool_id).first()
+    return tool
+
+@app.post('/tools',response_model=Tool,
+        status_code=status.HTTP_201_CREATED)
+def create_an_tool(tool:Tool):
+
+    db_item = db.query(models.Tool).filter(models.Tool.title == tool.title).first()
+
+    if db_item is not None:
+        raise HTTPException(status_code=400, details="Tool already exists")
+
+    new_tool = models.Tool(
+            title=tool.title,
+            description = tool.description,
+            image = tool.image,
+            link = tool.link
+            )
+
+    db.add(new_tool)
+    db.commit()
+
+    return new_tool
+
+@app.put('/tool/{tool_id}', response_model=Tool,
+        status_code=status.HTTP_200_OK)
+def update_an_tool(tool_id:int,tool:Tool):
+    tool_to_update = db.query(models.Tool).filter(models.Tool.id == tool_id).first()
+    tool_to_update.title = tool.title
+    tool_to_update.description = tool.description
+    tool_to_update.image = tool.image
+    tool_to_update.link = tool.link
+
+    db.commit()
+
+    return tool_to_update
+
+@app.delete('/tool/{tool_id}', response_model=Tool,
+        status_code=status.HTTP_200_OK)
+def delete_tool(tool_id:int):
+    tool_to_delete = db.query(models.Tool).filter(models.Tool.id == tool_id).first()
+
+    if tool_to_delete is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, details = "Resource Not Found")
+
+    return tool_to_delete
+
---
+++ models.py
@@ -0,0 +1,13 @@
+from database import Base
+from sqlalchemy import Text,Integer,Column
+
+class Tool(Base):
+    __tablename__='tools'
+    id = Column(Integer, primary_key=True)
+    title = Column(Text, nullable=False)
+    description = Column(Text, nullable=False)
+    image = Column(Text, nullable=False)
+    link = Column(Text, nullable=False)
+
+def __repr__(self):
+    return f"<Tool title={self.title} description={self.description} image={self.image} li:nk={self.link}>"
