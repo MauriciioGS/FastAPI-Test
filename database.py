9dce8724ac7d12f8da80ff3640df4a9e4f8c816a
H Mauricio García <30942047+MauriciioGS@users.noreply.github.com>
2021-12-30 18:07:00 -0600

    Update database.py

--- database.py
+++ database.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import sessionmaker
 #        echo=True
 #)
 sqlalchemy.engine.url.URL
-engine = create_engine("postgres://owncyrupydzcje:f2c62c2a1a93bb1d10eba1730ef05439cb171043838bcfefcec487a7fdc1008e@ec2-34-236-87-247.compute-1.amazonaws.com:5432/dbjigarbn8mqup",
+engine = create_engine("postgresql://owncyrupydzcje:f2c62c2a1a93bb1d10eba1730ef05439cb171043838bcfefcec487a7fdc1008e@ec2-34-236-87-247.compute-1.amazonaws.com:5432/dbjigarbn8mqup",
         echo=True
 )
 
